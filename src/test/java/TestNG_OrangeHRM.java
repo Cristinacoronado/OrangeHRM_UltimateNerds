fc0c5f4756b4cfc7622fb7c11ecaa4debdb1c19c
Kanokphorn Kala <kanokphornkala@Kanokphorns-MacBook-Pro.local>
2020-08-09 18:44:15 -0400

    add class

---
+++ src/test/java/TestNG_OrangeHRM.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_OrangeHRM {
+    WebDriver driver = null;
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
+    }
+
+
+
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+
+}
